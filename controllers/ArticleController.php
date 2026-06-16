569edc9aee5b4702d89a5ab73a5b34d9a57df0a2
hugo <hugo.blancan@outlook.fr>
2026-06-16 12:38:40 +0200

    ajout de la methode incrementViews dans ArticleManager, appel de cette methode dans ArticleController. Compteur de vues OK

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -31,6 +31,9 @@ class ArticleController
             throw new Exception("L'article demandé n'existe pas.");
         }
 
+        // Incrémentation du compteur de vues de l'article.
+        $articleManager->incrementViews($id);
+        
         $commentManager = new CommentManager();
         $comments = $commentManager->getAllCommentsByArticleId($id);
 
